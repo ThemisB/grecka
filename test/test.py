62483a668de43c7ae285a60ebb113a555d211a74
Themis Beris <themis.n.beris@gmail.com>
2018-08-04 13:49:10 +0300

    The single test file

---
+++ test/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("../lib")
+from grecka import Grecka
+
+if __name__ == "__main__":
+    print( Grecka.toGreeklish('η αθήνα είναι η πρωτεύουσα και μεγαλύτερη πόλη της ελλάδας. είναι από τις παλαιότερες πόλεις του κόσμου, με την καταγεγραμμένη ιστορία της να φθάνει ως το 3.200 π.χ.') )
